6fbf424603af93ed3baffd14bd9892ef3c6dd909
luoruiqing <lrq184187664@163.com>
2020-12-01 16:08:53 +0800

    celery 预置自定义任务/ 孤儿权限清除定义

--- django2.2/demo/conf/celery.py
+++ django2.2/demo/conf/celery.py
@@ -1,13 +1,26 @@
 import djcelery
+from celery.schedules import crontab
 from .default import INSTALLED_APPS, CACHE_URL, TIME_ZONE
 
-CELERY_TIMEZONE = TIME_ZONE
+# https://kinegratii.github.io/2015/10/22/djcelery-practice/
+
+CELERY_TIMEZONE = TIME_ZONE  # 任务时区
 BROKER_URL = CACHE_URL
+# CELERY_IMPORTS = ('.my_celery.tasks', ) # 任务定义所在的模块
+
 CELERYBEAT_SCHEDULER = 'djcelery.schedulers.DatabaseScheduler'
 
 INSTALLED_APPS += (
     'djcelery',  # 加入djcelery应用
 )
 
+
+CELERYBEAT_SCHEDULE = {  # 启动时自动写入库中
+    '定时清除孤儿对象权限': {
+        'task': 'account.tasks.clean_orphan_obj_perms',
+        'schedule': crontab(minute=0, hour=0),
+        "args": ()
+    },
+}
 
 djcelery.setup_loader()
