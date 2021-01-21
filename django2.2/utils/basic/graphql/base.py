259ff9b295126417f07aa891171134fbbb0b945d
luoruiqing <lrq184187664@163.com>
2021-01-21 19:18:34 +0800

    graphql 增加验证以及历史的写入

--- django2.2/utils/basic/graphql/base.py
+++ django2.2/utils/basic/graphql/base.py
@@ -1,8 +1,9 @@
 import graphene
+import functools
+import reversion
 from graphene.types import generic  # 通用字段
 from graphene_django import DjangoObjectType
 from django.utils.functional import cached_property
-
 
 class GrapBase:
     def executor(self, info, args, **kwargs):
@@ -83,3 +84,21 @@ class GrapDjangoModelDeleteBase(GrapDjangoModelMutationBase):
     ''' Django Model 删除基础 '''
 
     FLAG = 'Delete'
+
+
+
+def login_required(func):
+    ''' 未登录返回空 '''
+    @functools.wraps(func)
+    def warpper(self, info, args, **kwargs):
+        if not args.context.user.is_authenticated:
+            return None
+        with reversion.create_revision():  # 记录历史
+            result = func(self, info, args, **kwargs) # 执行
+            reversion.set_user(args.context.user)
+            reversion.set_comment(f"[Grap] 变更.")  # 设置版本的标注
+        return result
+    return warpper
+
+
+
